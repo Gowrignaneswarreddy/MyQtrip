5df1955f36f575d45c3e07afe06037e17868ac37
Gowrignaneswarreddy <gowrignaneswarreddy@gmail.com>
2024-10-07 11:12:32 +0530

    my new project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routing from './Routing';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+ <>
+   <Routing/>
+ </>
+);
+
+
